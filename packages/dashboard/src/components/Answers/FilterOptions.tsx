b0afd5ebf43b9fd52c7a82cac258553b28ea9eb8
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2020-03-05 23:32:04 +0200

    partially implemented filtering options to answers

--- packages/dashboard/src/components/Answers/FilterOptions.tsx
+++ packages/dashboard/src/components/Answers/FilterOptions.tsx
@@ -1,4 +1,19 @@
-import { Grid, Typography } from "@material-ui/core"
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  FormLabel,
+  Grid,
+  InputLabel,
+  List,
+  ListItem,
+  Select,
+  TextField,
+  Typography,
+} from "@material-ui/core"
 import React from "react"
 
 const FilterBox = ({ numberOfAnswers }) => {
@@ -36,17 +51,207 @@ const FilterBox = ({ numberOfAnswers }) => {
   )
 }
 
-const QuizAnswersFilterSelection: React.FunctionComponent<any> = () => {
+type QuizAnswerState =
+  | "submitted"
+  | "confirmed"
+  | "spam"
+  | "rejected"
+  | "deprecated"
+
+interface IQuizAnswersFilterSelectionState {
+  minDate: null | string
+  maxDate: null | string
+  states: [QuizAnswerState?]
+  minSpamFlags: null | number
+  maxSpamFlags: null | number
+  minGivenPeerReviews: null | number
+  maxGivenPeerReviews: null | number
+  minReceivedPeerReviews: null | number
+  maxReceivedPeerReviews: null | number
+  minAverageOfGrades: null | number
+  maxAverageOfGrades: null | number
+}
+
+class QuizAnswersFilterSelection extends React.Component<
+  any,
+  IQuizAnswersFilterSelectionState
+> {
+  constructor(props) {
+    super(props)
+
+    const date = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000)
+
+    this.state = {
+      minDate: null,
+      maxDate: date.toLocaleDateString(),
+      states: ["submitted"],
+      minSpamFlags: null,
+      maxSpamFlags: null,
+      minGivenPeerReviews: null,
+      maxGivenPeerReviews: null,
+      minReceivedPeerReviews: null,
+      maxReceivedPeerReviews: null,
+      minAverageOfGrades: null,
+      maxAverageOfGrades: null,
+    }
+  }
+
+  public render() {
+    return (
+      <React.Fragment>
+        <List>
+          <ListItem>
+            <Typography>Age</Typography>
+
+            <TextField
+              label="min"
+              type="datetime-local"
+              value={this.state.minDate != null ? this.state.minDate : ""}
+              onChange={this.modifyField("minDate")}
+            />
+            <TextField
+              label="max"
+              type="datetime-local"
+              value={this.state.maxDate != null ? this.state.maxDate : ""}
+              onChange={this.modifyField("maxValue")}
+            />
+          </ListItem>
+
+          <ListItem>
+            <FormControl>
+              <FormLabel>Status</FormLabel>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={false}
+                      onChange={testFunction}
+                      value="status1"
+                    />
+                  }
+                  label="Status no 1"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={true}
+                      onChange={testFunction}
+                      value="status2"
+                    />
+                  }
+                  label="Status no2"
+                />
+              </FormGroup>
+            </FormControl>
+          </ListItem>
+
+          <ListItem>
+            Spam flags:
+            <IntegerSelection
+              min={this.state.minSpamFlags}
+              max={this.state.maxSpamFlags}
+              changeMax={this.modifyField("maxSpamFlags")}
+              changeMin={this.modifyField("minSpamFlags")}
+            />
+          </ListItem>
+
+          <ListItem>
+            Given peer reviews:
+            <IntegerSelection
+              min={this.state.minGivenPeerReviews}
+              max={this.state.maxGivenPeerReviews}
+              changeMax={this.modifyField("maxGivenPeerReviews")}
+              changeMin={this.modifyField("minGivenPeerReviews")}
+            />
+          </ListItem>
+
+          <ListItem>
+            Received peer reviews:
+            <IntegerSelection
+              min={this.state.minReceivedPeerReviews}
+              max={this.state.maxReceivedPeerReviews}
+              changeMax={this.modifyField("maxReceivedPeerReviews")}
+              changeMin={this.modifyField("minReceivedPeerReviews")}
+            />
+          </ListItem>
+
+          <ListItem>
+            Average of grades:
+            <IntegerSelection
+              min={this.state.minAverageOfGrades}
+              max={this.state.maxAverageOfGrades}
+              changeMax={this.modifyField("maxAverageOfGrades")}
+              changeMin={this.modifyField("minAverageOfGrades")}
+            />
+          </ListItem>
+        </List>
+
+        <Button variant="outlined" color="primary">
+          Filter
+        </Button>
+      </React.Fragment>
+    )
+  }
+
+  private modifyField = (fieldName: string) => (e: any) => {
+    const newState = { ...this.state }
+    newState[fieldName] = e.target.value
+    this.setState(newState)
+  }
+}
+
+const testFunction = e => {
+  console.log(
+    "Something happened, here's the event target value: ",
+    e.target.value,
+  )
+}
+
+class Selection extends React.Component<any, any> {
+  constructor(props) {
+    super(props)
+  }
+
+  public render() {
+    return <div>yeet</div>
+  }
+}
+
+interface IntegerSelectionProps {
+  min: null | number
+  max: null | number
+  changeMin: (e: any) => void
+  changeMax: (e: any) => void
+}
+
+const IntegerSelection: React.FunctionComponent<IntegerSelectionProps> = ({
+  min,
+  changeMin,
+  max,
+  changeMax,
+}) => {
   return (
-    <ul>
-      <li>Age (both ends settable)</li>
-      <li>Status (multiple selection)</li>
-      <li>Spam flags (more than, fewer than than)</li>
-      <li>Given peer reviews (more than, fewer than)</li>
-      <li>Received peer reviews(more than, fewer than)</li>
-      <li>Average of all grades (more, less)</li>
-      <li>Other calculated statistical numbers</li>
-    </ul>
+    <FormGroup row={true}>
+      <TextField
+        variant="outlined"
+        fullWidth={false}
+        label={"Min:"}
+        value={min != null ? min : ""}
+        type="number"
+        onChange={changeMin}
+        style={{ maxWidth: "40%" }}
+      />
+
+      <TextField
+        variant="outlined"
+        fullWidth={false}
+        label={"Max:"}
+        value={max != null ? max : ""}
+        type="number"
+        onChange={changeMax}
+        style={{ marginLeft: "12px", maxWidth: "40%" }}
+      />
+    </FormGroup>
   )
 }
 
